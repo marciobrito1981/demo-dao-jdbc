f5b6d6348d76f870d943005a16e82bf412dd4938
Marcio Brito <marcio.brito@outlook.com>
2020-04-16 11:38:22 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,8 +38,15 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted ! New id = " + newSeller.getId());
-		}
 		
-		
+		System.out.println();
+		System.out.println("=== TEST 5: Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+	
+	
 	}
+}
 
