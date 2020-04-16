e7208dd364d93590afa9862f738c16100cccf6a5
Marcio Brito <marcio.brito@outlook.com>
2020-04-16 11:26:27 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,7 +33,13 @@ public class Program {
 			System.out.println(s);
 		}
 		
+		System.out.println();
+		System.out.println("=== TEST 4: Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted ! New id = " + newSeller.getId());
+		}
+		
 		
 	}
 
-}
